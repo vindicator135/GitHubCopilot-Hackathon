4dbed75ea2a585db89b5a736d46157281374e6ef
Alvin Vinalon <alvin.vinalon@avanade.com>
2024-10-18 15:50:55 +1100

    Add Jest as a devDependency and test script in package.json Refactor demo.js and add comments for different demos

---
+++ exercise/javascript/demo.js
@@ -0,0 +1,23 @@
+// Demo 1:
+// Question: What are the popular JavaScript testing frameworks?
+// Answer:
+
+// Demo 2:
+// Generate a function that validates a credit card number.
+// The function will return if its VISA, MasterCard, American Express, Discover.
+
+// Inline Chat: If not suggested, ask GHCP to add a more elegant error handler to the credit card function above using Ctrl + i (Inline chat)
+
+// Inline chat prompt: Rather than throwing error when card number is invalid, I want a more elegant error message.
+
+// Demo 3:
+// Test the Credit Card function using the console.
+//console.log(validateCreditCard("4712303894023455")); // Expected: "VISA"
+
+// How to run the test:
+
+// Demo 4:
+// Copilot Chat: I want to use JEST framework for testing my application.
+
+// Unit Test the Credit Card function using /tests
+// Export the function to be used in the test file.
